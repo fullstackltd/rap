91546c244fcd72053acf2986adedfbef1bdffb86
fullstackltd <fullstackltd@gmail.com>
2023-11-10 15:20:26 +0600

    09/11/23

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  const [count, setCount] = useState<number>(0);
+
+  const update = (v: number) => (): void => {
+    setCount((prev: number) => prev + v);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={update(+1)}>+</button>
+      <button disabled={count === 0} onClick={update(-1)}>-</button>
+      <button onClick={update(-count)}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
